8760457495c252e4a03995406c4591fcf183575a
Charles Poullet <CPoullet@spartaglobal.com>
2020-02-05 10:53:11 +0000

    Added some methods

--- homework1.py
+++ homework1.py
@@ -14,21 +14,21 @@ species = 'Human'
 eye_colour = 'Blue'
 hair_colour = 'Blonde'
 
-print('Hello there! Please input your first name:  ')
-name = input()
+print('Hello there! Please input your first name:')
+name = input().strip().capitalize()
 
-print('Last name:   ')
-last_name = input()
+print('Last name:')
+last_name = input().strip().capitalize()
 
-print('Your eye colour: ')
-eye_colour = input()
+print('Your eye colour:')
+eye_colour = input().strip().lower()
 
-print('Hair colour:  ')
-hair_colour = input()
+print('Hair colour:')
+hair_colour = input().strip().lower()
 
-print('Age:   ')
-age = int(input())
+print('Age:')
+age = int(input().strip())
 
 year_born = 2020 - age
 
-print(f"Your name is {name} {last_name}, your eye colour is {eye_colour}, your hair is {hair_colour}, you are {age} years old and were born in {year_born}.")
+print(f"Your name is {name} {last_name}, your eye colour is {eye_colour}, your hair is {hair_colour}, you are {age} years old and was born in {year_born}.")
