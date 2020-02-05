8aa3d5599fd3396fb2975e8d5baf7112bd1a3f68
chpoullet <59974805+chpoullet@users.noreply.github.com>
2020-02-05 09:24:49 +0000

    Update homework1.py

--- homework1.py
+++ homework1.py
@@ -5,6 +5,7 @@
 # name = input('what new name would you like?')
 # Print them back to the user as conversation
 # Example: 'Hello Jack! Welcome, your age is 26, your eyes are green and your hair color is black.
+
 #Section 2 - Calculate in what year was the person born? and responde back.
 # print something like: 'You said you we're 28 hence you were born in 1991!'
 
